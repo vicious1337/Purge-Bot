24495fd700af56b784c0458ba5e0bd1316fb3357
vicious <69701057+vicious1337@users.noreply.github.com>
2021-01-30 10:49:39 -0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,26 @@
+prefix = "!"
+token = "TOKEN HERE"
+
+import asyncio
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+
+client = commands.Bot(command_prefix=prefix, self_bot=True)
+
+@client.event
+async def on_ready():
+    print ("Username: {}#{}".format(client.user.name, client.user.discriminator))
+    print ("How to use: !purge [amount]")
+    print ("Creator: github.com/vicious1337")
+
+@client.command()
+async def purge(ctx, amount: int): # b'\xfc'
+    await ctx.message.delete()
+    async for message in ctx.message.channel.history(limit=amount).filter(lambda m: m.author == client.user).map(lambda m: m):
+        try:
+           await message.delete()
+        except:
+            pass
+
+client.run(token, bot=False)
---
+++ launch.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title Purge Bot
+py -3.8 Main.py
+echo Purge bot has crashed
+pause
